79e6aa6518a7f246c534a4a82f1a41b760b799f9
Bob Florian <bob@smartthings.com>
2019-06-13 11:23:37 -0700

    Refactored SDK 1

--- routes/schema.js
+++ routes/schema.js
@@ -5,152 +5,118 @@ const _ = require("lodash");
 const db = require('../lib/db');
 const deviceService = require('../lib/devices');
 const mapping = require('../lib/mapping');
+const SchemaConnector = require("../lib/schema-connector")
 
 const { partnerHelper, CommandResponse } = require("st-schema");
 const stPartnerHelper = new partnerHelper({}, {});
 
-router.post('/', async function (req, res) {
-  console.log("REQUEST: %s", JSON.stringify(req.body, null, 2));
-
-  let response;
-  const {headers, authentication, devices} = req;
-  const {interactionType1, requestId} = headers;
-  const interactionType = req.body.headers.interactionType;
-
-  try {
-    switch (interactionType) {
-      case "discoveryRequest":
-        response = await discoveryRequest(req);
-        break;
-      case "commandRequest":
-        response = await commandRequest(req);
-        break;
-      case "stateRefreshRequest":
-        response = await stateRefreshRequest(req);
-        break;
-      case "grantCallbackAccess":
-        response = await grantCallbackAccess(req);
-        break;
-      case "integrationDeleted":
-        response = await integrationDeletedRequest(req);
-        break;
-      default:
-        response = "ERROR. Unsupported interactionType: " + interactionType;
-        console.log(response);
-        break;
-    }
-  } catch (ex) {
-    console.log("failed with ex", ex)
-  }
-
-  console.log("RESPONSE: %s", JSON.stringify(response, null, 2));
-  res.send(response);
-});
-
-async function discoveryRequest(request) {
-  const devices = (await db.getDevicesForToken(request.body.authentication.token)).map(device => {
-    return {
-      "externalDeviceId": device.externalId,
-      "friendlyName": device.displayName,
-      "deviceHandlerType": device.handlerType,
-      "manufacturerInfo": {
-        "manufacturerName": "SmartThings",
-        "modelName": "Virtual Viper device",
-        "hwVersion": "v1",
-        "swVersion": "23.123.231"
-      }
-    }
-  });
-  const resp = {
-    "headers": {
-      "schema": "st-schema",
-      "version": "1.0",
-      "interactionType": "discoveryResponse",
-      "requestId": request.body.headers.requestId
-    },
-    "devices": devices
-  };
-  return resp
-}
-
-function integrationDeletedRequest(request) {
-  db.removeToken(request.body.authentication.token);
-  return {};
-}
-
-async function commandRequest(request) {
-  const ops = [];
-  const response = new CommandResponse(request.body.headers.requestId);
-  const account = await db.getAccountForToken(request.body.authentication.token);
-  request.body.devices.map(async ({ externalDeviceId, deviceCookie, commands }) => {
-
-    const device = response.addDevice(externalDeviceId, deviceCookie);
-    stPartnerHelper.mapSTCommandsToState(device, commands);
-
-    const states = mapping.externalStatesFor(commands);
-    if (states) {
-      ops.push(db.updateDeviceState(account.username, externalDeviceId, states));
-      deviceService.updateProactiveState(account.username, externalDeviceId, states)
-    }
-  });
-
-  await Promise.all(ops);
-
-  return response;
-}
-
-async function stateRefreshRequest(request) {
-  // TODO Check against devices in request ?
-  const deviceState = (await db.getDevicesForToken(request.body.authentication.token)).map(device => {
-    return {
-      "externalDeviceId": device.externalId,
-      "states": mapping.stRefreshStatesFor(device.states)
-    }
-  });
-
-  return {
-    "headers": {
-      "schema": "st-schema",
-      "version": "1.0",
-      "interactionType": "stateRefreshResponse",
-      "requestId": request.body.headers.requestId
-    },
-    "deviceState": deviceState
-  }
-}
-
-async function grantCallbackAccess(request) {
-  const auth  = request.body.callbackAuthentication;
-  if (auth.clientId === process.env.ST_CLIENT_ID ) {
-    const options = {
-      url: request.body.callbackUrls.oauthToken,
-      method: 'POST',
-      json: true,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      body: {
-        "headers": {
-          "schema": "st-schema",
-          "version": "1.0",
-          "interactionType": "accessTokenRequest",
-          "requestId": request.body.headers.requestId
-        },
-        "callbackAuthentication": {
-          "grantType": "authorization_code",
-          "code": auth.code,
-          "clientId": process.env.ST_CLIENT_ID,
-          "clientSecret": process.env.ST_CLIENT_SECRET
+const connector = new SchemaConnector()
+  .clientId(process.env.ST_CLIENT_ID)
+  .clientSecret(process.env.ST_CLIENT_SECRET)
+  .discoveryHandler(async data => {
+    const devices = (await db.getDevicesForToken(data.authentication.token)).map(device => {
+      return {
+        "externalDeviceId": device.externalId,
+        "friendlyName": device.displayName,
+        "deviceHandlerType": device.handlerType,
+        "manufacturerInfo": {
+          "manufacturerName": "SmartThings",
+          "modelName": "Virtual Viper device",
+          "hwVersion": "v1",
+          "swVersion": "23.123.231"
         }
       }
+    });
+    return {
+      "headers": {
+        "schema": "st-schema",
+        "version": "1.0",
+        "interactionType": "discoveryResponse",
+        "requestId": data.headers.requestId
+      },
+      "devices": devices
     };
+  })
+  .stateRefreshHandler(async data => {
+    // TODO Check against devices in request ?
+    const deviceState = (await db.getDevicesForToken(data.authentication.token)).map(device => {
+      return {
+        "externalDeviceId": device.externalId,
+        "states": mapping.stRefreshStatesFor(device.states)
+      }
+    });
 
-    const data = await rp(options);
-    console.log('AUTH RESPONSE: ' + JSON.stringify(data, null, 2));
+    return {
+      "headers": {
+        "schema": "st-schema",
+        "version": "1.0",
+        "interactionType": "stateRefreshResponse",
+        "requestId": data.headers.requestId
+      },
+      "deviceState": deviceState
+    }
+  })
+  .commandHandler(async data => {
+    const ops = [];
+    const response = new CommandResponse(data.headers.requestId);
+    const account = await db.getAccountForToken(data.authentication.token);
+    data.devices.map(async ({ externalDeviceId, deviceCookie, commands }) => {
 
-    db.setCallbackInfo(request.body.authentication.token, data.callbackAuthentication, request.body.callbackUrls)
-  }
-  return {};
-}
+      const device = response.addDevice(externalDeviceId, deviceCookie);
+      stPartnerHelper.mapSTCommandsToState(device, commands);
+
+      const states = mapping.externalStatesFor(commands);
+      if (states) {
+        ops.push(db.updateDeviceState(account.username, externalDeviceId, states));
+        deviceService.updateProactiveState(account.username, externalDeviceId, states)
+      }
+    });
+
+    await Promise.all(ops);
+
+    return response;
+  })
+  .grantCallbackAccessHandler(async data => {
+    const auth  = data.callbackAuthentication;
+    if (auth.clientId === process.env.ST_CLIENT_ID ) {
+      const options = {
+        url: data.callbackUrls.oauthToken,
+        method: 'POST',
+        json: true,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: {
+          "headers": {
+            "schema": "st-schema",
+            "version": "1.0",
+            "interactionType": "accessTokenRequest",
+            "requestId": data.headers.requestId
+          },
+          "callbackAuthentication": {
+            "grantType": "authorization_code",
+            "code": auth.code,
+            "clientId": process.env.ST_CLIENT_ID,
+            "clientSecret": process.env.ST_CLIENT_SECRET
+          }
+        }
+      };
+
+      const data = await rp(options);
+      console.log('AUTH RESPONSE: ' + JSON.stringify(data, null, 2));
+
+      db.setCallbackInfo(data.authentication.token, data.callbackAuthentication, data.callbackUrls)
+    }
+  })
+  .integrationDeletedHandler(data => {
+    db.removeToken(data.authentication.token);
+    return {};
+  })
+
+
+router.post('/', async function (req, res) {
+  console.log("REQUEST: %s", JSON.stringify(req.body, null, 2));
+  connector.handleHttpCallback(req, res)
+});
 
 module.exports = router;
